ee8b1d96af75d4d6c22f6146a5132c0d9eaf4088
Developers@Work <DevelopersWork@users.noreply.github.com>
2023-09-30 13:25:17 +0000

    update on project tag line

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,7 +12,7 @@ const Projects = ({openModal,setOpenModal}) => {
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+        I've engaged in a diverse array of projects throughout my journey. Here are some of my notable game-related projects.
         </Desc>
         <ToggleButtonGroup >
           {toggle === 'all' ?
